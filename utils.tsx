b8f8fb3efc85ae2bb89cc363cbcbe9a51e4bc616
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-04-14 18:15:40 +0200

    Red

--- utils.tsx
+++ utils.tsx
@@ -75,7 +75,7 @@ const XAxis = () => {
                     marginTop: 10,
                 }}
             >
-                Grey
+                Red
             </Text>
             <Text
                 style={{
